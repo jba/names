54fd1f50290a23bce71f259d031388f4505c2419
Jonathan Amsterdam <jba@google.com>
2020-12-04 19:32:33 -0500

    add some files

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/jba/names
+
+go 1.15
+
+require google.golang.org/grpc v1.29.0
---
+++ main.go
@@ -0,0 +1,7 @@
+package names
+
+import "google.golang.org/grpc/naming"
+
+func Resolve() naming.Watcher {
+	return nil
+}
